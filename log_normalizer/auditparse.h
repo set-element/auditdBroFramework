15e4c34b2850313a392f12b673abcdaeace4a65f
scott campbell <set.element@gmail.com>
2015-12-28 09:41:45 -0500

    add function prototype
    Add prototypes for functions and clean things up a bit

--- log_normalizer/auditparse.h
+++ log_normalizer/auditparse.h
@@ -25,56 +25,57 @@ struct file_struct {
 	int i_watch;		/* Inotify watch associated with file_struct */
 };
 
-#define PLACE_OBJ 1
-#define USER_OBJ  2
-#define SYSCALL_OBJ 3 
-#define SOCK_OBJ 4 
-#define EXECVE_OBJ 5 
+#define PLACE_OBJ   1
+#define USER_OBJ    2
+#define SYSCALL_OBJ 3
+#define SOCK_OBJ    4
+#define EXECVE_OBJ  5
 #define GENERIC_OBJ 6
 
 int user_types[] = {1100, 1101, 1102, 1103, 1105, 1106, 1108, 1109, 1112, 1113, 1122, 1123};
 int user_types_count = 12;
-#define F_A0 "a0"
-#define F_A1 "a1"
-#define F_A2 "a2"
-#define F_ARG "arg"
-#define F_ARGC "argc"
-#define F_AUID "auid"
-#define F_COMM "comm"
-#define F_CWD "cwd"
-#define F_EGID "egid"
-#define F_EUID "euid"
-#define F_EXE "exe"
-#define F_EXIT "exit"
-#define F_FLAVOR "flavor"
-#define F_FSGID "fsgid"
-#define F_FSUID "fsuid"
-#define F_GID "gid"
-#define F_INODE "inode"
-#define F_HEY "key"
-#define F_MODE "mode"
-#define F_MSG "msg"
-#define F_NAME "name"
-#define F_NODE "node"
-#define F_AGID "ogid"
-#define F_PID "pid"
-#define F_PPID "ppid"
-#define F_SADDR "saddr"
-#define F_SES "ses"
-#define F_SGID "sgid"
-#define F_SUCCESS "success"
-#define F_SUID "suid"
-#define F_SYSCALL "syscall"
+
+#define F_A0       "a0"
+#define F_A1       "a1"
+#define F_A2       "a2"
+#define F_ARG      "arg"
+#define F_ARGC     "argc"
+#define F_AUID     "auid"
+#define F_COMM     "comm"
+#define F_CWD      "cwd"
+#define F_EGID     "egid"
+#define F_EUID     "euid"
+#define F_EXE      "exe"
+#define F_EXIT     "exit"
+#define F_FLAVOR   "flavor"
+#define F_FSGID    "fsgid"
+#define F_FSUID    "fsuid"
+#define F_GID      "gid"
+#define F_INODE    "inode"
+#define F_HEY      "key"
+#define F_MODE     "mode"
+#define F_MSG      "msg"
+#define F_NAME     "name"
+#define F_NODE     "node"
+#define F_AGID     "ogid"
+#define F_PID      "pid"
+#define F_PPID     "ppid"
+#define F_SADDR    "saddr"
+#define F_SES      "ses"
+#define F_SGID     "sgid"
+#define F_SUCCESS  "success"
+#define F_SUID     "suid"
+#define F_SYSCALL  "syscall"
 #define F_TERMINAL "terminal"
-#define F_TIME "time"
-#define F_TTY "tty"
-#define F_TYPE "type"
-#define F_UID "uid"
-#define F_FLAVOR "flavor"
-#define F_OUID "ouid"
-#define F_OGID "ogid"
-#define F_TERM "terminal"
-#define F_KEY "key"
+#define F_TIME     "time"
+#define F_TTY      "tty"
+#define F_TYPE     "type"
+#define F_UID      "uid"
+#define F_FLAVOR   "flavor"
+#define F_OUID     "ouid"
+#define F_OGID     "ogid"
+#define F_TERM     "terminal"
+#define F_KEY      "key"
 
 #define IS_PIPELIKE(mode) \
 	(S_ISFIFO(mode) || S_ISSOCK(mode))
@@ -98,3 +99,8 @@ int user_types_count = 12;
 #endif /* __GNUC__ */
 
 #endif /* _INOTAIL_H */
+
+static int tail_file(struct file_struct *f, unsigned long n_units, char mode, char forever);
+static int handle_inotify_event(struct inotify_event *inev, struct file_struct *f);
+static int watch_files(struct file_struct *files, int n_files);
+
